86bb0677c770798ffb8070f1c475ab2143fcca5f
FelipeBastosxj <55720172+FelipeBastosxj@users.noreply.github.com>
2021-02-23 22:19:23 -0300

    atualização

--- js/f-projeto.js
+++ js/f-projeto.js
@@ -1,9 +1,14 @@
 logo = document.querySelector('.pegar')
 
 console.log(logo)
+setTimeout(() =>{
+   logo.classList.add('mostrando')
+
+},1)
+
 setInterval(() =>{
+
    logo.classList.toggle('mostrando')
    logo.classList.toggle('mostrando2')
    
 },3000)
-
