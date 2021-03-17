9b5b3688791b19c4d5bc0e789b30b56f0656a53e
FelipeBastosxj <55720172+FelipeBastosxj@users.noreply.github.com>
2021-03-17 12:02:42 -0300

    First-project-desktop-part
    Parte desktop finalizada

--- js/f-projeto.js
+++ js/f-projeto.js
@@ -1,3 +1,5 @@
+/* SLIDER INICIAL*/
+
 logo = document.querySelector('.pegar')
 
 setTimeout(() =>{
@@ -11,3 +13,52 @@ setInterval(() =>{
    logo.classList.toggle('mostrando2')
    
 },5000)
+
+
+/* ANIMA CLICK DESCIDA*/
+
+let sessoes = document.querySelectorAll(".p")
+
+function Ascroll(event){
+   event.preventDefault()
+
+   href = event.currentTarget.getAttribute('href')
+
+   elemento = document.querySelector(href)
+
+   console.log(elemento)
+
+   
+  elemento.scrollIntoView({
+     block: 'start', 
+     behavior: "smooth"
+  })
+
+}
+
+sessoes.forEach(e => {
+   e.addEventListener('click', Ascroll)
+});
+
+/* SCROLL DECENDO APARECER */
+
+let aparecer = document.querySelectorAll('.s')
+
+function Sscroll(){
+   aparecer.forEach(e =>{
+     altura = e.getBoundingClientRect().top
+
+      if(altura < 350){
+         e.classList.add('ativo')
+      }else{
+         e.classList.remove('ativo')
+      }
+   })
+ 
+}
+
+
+
+window.addEventListener('scroll', Sscroll)
+
+
